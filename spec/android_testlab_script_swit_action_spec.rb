4593561380c690c40bce78a397a234b88b59d0e6
나비이쁜이 <munkijung1992@gmail.com>
2023-08-25 09:42:56 +0900

    0.1.2

--- spec/android_testlab_script_swit_action_spec.rb
+++
@@ -1,9 +0,0 @@
-describe Fastlane::Actions::AndroidTestlabScriptSwitAction do
-  describe '#run' do
-    it 'prints a message' do
-      expect(Fastlane::UI).to receive(:message).with("The android_testlab_script_swit plugin is working!")
-
-      Fastlane::Actions::AndroidTestlabScriptSwitAction.run(nil)
-    end
-  end
-end
