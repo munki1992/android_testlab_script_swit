af414abfcd40a298ef30aca3977cb71ad61978c8
나비이쁜이 <munkijung1992@gmail.com>
2023-08-25 18:25:23 +0900

    0.1.44

--- lib/fastlane/plugin/android_testlab_script_swit/helper/android_testlab_script_swit_helper.rb
+++ lib/fastlane/plugin/android_testlab_script_swit/helper/android_testlab_script_swit_helper.rb
@@ -32,7 +32,7 @@ module Fastlane
       Action.sh("gcloud auth activate-service-account --key-file #{gcloud_key_file}")
     end
 
-    def self.run_tests(arguments)
+    def self.run_tests(gcloud_components_channel, arguments)
       UI.message("Test running...")
       Action.sh("set +e; gcloud#{' ' + gcloud_components_channel unless gcloud_components_channel == "stable"} firebase test android run #{arguments}; set -e")
 #       Action.sh("set +e; gcloud #{gcloud_components_channel unless gcloud_components_channel == "stable"} firebase test android run #{arguments}; set -e")
